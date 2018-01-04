d88cf04c176b878342a4bbd5d2fce78258407ed7
hu <a1398@hu-MacBook-Pro.local>
2018-01-05 03:51:16 +0800

    Initial commit

---
+++ app/src/main/java/com/example/hu/dazuoye/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.hu.dazuoye;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //主界面返回键重置为回到桌面
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        startActivity(intent);
+
+    }
+}
---
+++ app/src/main/java/com/example/hu/dazuoye/Register.java
@@ -0,0 +1,18 @@
+package com.example.hu.dazuoye;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by a1398 on 2018/1/5.
+ */
+
+public class Register extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+    }
+}
